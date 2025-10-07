02bd82f5919ec6a155aaf65d5927d18d6bf07136
lwidajewski <lwidajew@umich.edu>
2025-10-07 16:36:04 -0400

    Updated JS and HTML
    Added a 'clear completed' button that will remove goals that were checked.

--- Javascrip/main.js
+++ Javascrip/main.js
@@ -7,25 +7,29 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
-// viewGoals page
+// viewGoals page - addDeleteGoals function
 // pass in id values to function
-addGoal("addDailyBtn", "dailyTextInput");
-addGoal("addShortBtn", "shortTextInput");
-addGoal("addLongBtn", "longTextInput");
+addDeleteGoals("addDailyBtn", "dailyTextInput", "dailyGoals", "clearDailyBtn");
+addDeleteGoals("addShortBtn", "shortTextInput", "shortTermGoals", "clearShortBtn");
+addDeleteGoals("addLongBtn", "longTextInput", "longTermGoals", "clearLongBtn");
 
-function addGoal(btnID, inputID){
-  const addBtn = document.getElementById(btnID); // uses the string to get specific element
-  const textInput = document.getElementById(inputID)
+function addDeleteGoals(btnID, inputID, containerID, clearBtnID){
+  // uses the string passed to get specific element
+  const addBtn = document.getElementById(btnID);
+  const textInput = document.getElementById(inputID); // textbox input
+  const containerLocation = document.getElementById(containerID); // this is the specific div element we are in
+  const clearBtn = document.getElementById(clearBtnID);
 
-  // checks for a click of the button and then does stuff
+  // checks for a click of the ADD button and then does stuff
   addBtn.addEventListener("click", () => {
     // remove whitespace
     const filteredTextInput = textInput.value.trim();
     // check if user entered nothing
     if (filteredTextInput === ""){
       return;
-    }else{
-      // create div, checkbox, and label elements with appropriate classes
+    }
+    else{
+      // create div, checkbox, and label elements with appropriate html/CSS classes
       const goalDiv = document.createElement("div");
       
       const checkBox = document.createElement("input");
@@ -36,14 +40,20 @@ function addGoal(btnID, inputID){
       label.classList.add("cb-label");
       label.textContent = filteredTextInput // add input from user to label
 
-      // add to html
+      // add to html in specific div location from earlier
       goalDiv.appendChild(checkBox);
       goalDiv.appendChild(label);
-
-      addBtn.parentElement.before(goalDiv);
+      containerLocation.appendChild(goalDiv);
 
       // reset textbox used
       textInput.value = "";
     };
+  });
+  // check for CLEAR button click
+  clearBtn.addEventListener("click", () => {
+    // find every checkbox that is checked in that specific container (div) and remove it
+    containerLocation.querySelectorAll(".strikethrough:checked").forEach(cb => {
+      cb.parentElement.remove();
+    });
   });
 };
